42775ee2aaf71fabf568be3838e26b593dfe5820
rafalgorniak <234753@edu.p.lodz.pl>
2022-09-14 11:00:02 +0200

    14.09 comm

--- js1/assets/scripts/app.js
+++ js1/assets/scripts/app.js
@@ -1,38 +1,68 @@
 const defaultValue = 0;
 let currentResult = defaultValue;
+LOG = [];
 
+//creating method responsible for refreshing and actualizing site data
 function createAndWriteOutput(operator, resultBefore, calcNumber) {
     const calcDescription = `${resultBefore} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+function inputvalue() {
+    if(userInput.value!="") {
+        return parseInt(userInput.value);
+    }
+    return 0;
+}
+
+function makelog(operation, prevres, numb, curres) {
+    const logEntry = {
+        operation: operation,
+        prevResult: prevres,
+        number: numb,
+        result: curres
+    };
+    LOG.push(logEntry);
+    console.log(logEntry);
+}
+
 function add() {
     const numberBefore = currentResult;
-    const enteredNumber = parseInt(userInput.value);
+    const enteredNumber = inputvalue();
     currentResult = currentResult + enteredNumber;
-    createAndWriteOutput('+',numberBefore,enteredNumber)
+    createAndWriteOutput('+',numberBefore,enteredNumber);
+    makelog('addition',numberBefore,enteredNumber,currentResult);
 }
 
 function substract() {
     const numberBefore = currentResult;
-    const enteredNumber = parseInt(userInput.value);
-    currentResult = currentResult - parseInt(userInput.value);
-    createAndWriteOutput('-',numberBefore,enteredNumber)
+    const enteredNumber = inputvalue()
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-',numberBefore,enteredNumber);
+    makelog('substraction',numberBefore,enteredNumber,currentResult);
 }
+
 
 function multiply() {
     const numberBefore = currentResult;
-    const enteredNumber = parseInt(userInput.value);
-    currentResult = currentResult * parseInt(userInput.value);
-    createAndWriteOutput('*',numberBefore,enteredNumber)
+    const enteredNumber = inputvalue()
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*',numberBefore,enteredNumber);
+    makelog('multiplying',numberBefore,enteredNumber,currentResult);
 }
+
 
 function divide() {
     const numberBefore = currentResult;
-    const enteredNumber = parseInt(userInput.value);
-    currentResult = currentResult / parseInt(userInput.value);
-    createAndWriteOutput('/',numberBefore,enteredNumber)
+    const enteredNumber = inputvalue()
+    if(enteredNumber!=0) {
+        currentResult = currentResult / enteredNumber;
+        createAndWriteOutput('/',numberBefore,enteredNumber);
+        makelog('division',numberBefore,enteredNumber,currentResult);
 }
+} 
+
+
 
 addBtn.addEventListener('click',add);
 substractBtnnput.addEventListener('click',substract);
